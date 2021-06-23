3d6dba4e960d2a54e43c3b432312f0c345adb04c
Jitendra Soni <jitusonijk@gmail.com>
2021-06-23 18:33:26 +1000

    Initial commit for the fullstack - Basic HTML Structure

---
+++ js/demo.js
@@ -0,0 +1,19 @@
+ function changeImage() {
+            var image = document.getElementById('myImage');
+     
+	 alert('test');
+                image.src = "img/kirtan.png";
+    
+        };
+		
+		 function ShowWeather() {
+          
+		  
+		  var weather = document.getElementById('weather');
+     
+	 weather.value ="Function called";
+     
+	
+              
+    
+        }
