091ae775aedecddf7c3070ad3afd9acaf269c8db
jitendrasoni <jitusonijk@gmail.com>
2021-07-10 20:34:02 +1000

    local changes for the javascript

---
+++ test/Harsh/Basic JavaScript/demo.js
@@ -0,0 +1,6 @@
+function test()
+{
+    var username = document.getElementById('value1').value;
+    var password = document.getElementById('value2').value;
+    alert(parseInt(username) + parseInt(password) );
+}
